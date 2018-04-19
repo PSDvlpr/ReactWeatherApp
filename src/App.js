f90751d393260e6dec5ddf1a4a0fb83e1600c73d
Piotr Strzelecki <strzelecki.piotras@gmail.com>
2018-04-19 22:06:59 +0200

    Add API functionality to search cities and countries

--- src/App.js
+++ src/App.js
@@ -1,13 +1,29 @@
 import React, { Component } from 'react';
 import Title from './components/Title';
 import Form from './components/Form';
+import Weather from './components/Weather';
+
+const API_KEY = '54c9652acba7ffc14be90f916d42b95c';
 
 class App extends Component {
+
+  getWeather = async (e) => {
+    e.preventDefault();
+
+    const city = e.target.elements.city.value;
+    const country = e.target.elements.country.value;
+    const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&units=metric&appid=${API_KEY}`);
+    const data = await api_call.json();
+    console.log(data);
+  }
+
+
   render() {
     return (
       <div>
         <Title />
-        <Form />
+        <Form getWeather={this.getWeather} />
+        <Weather />
       </div>
 
     );
